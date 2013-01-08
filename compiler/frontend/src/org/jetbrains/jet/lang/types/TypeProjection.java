31b04580c0f685d1bc53ddc7ebe87f963e9d22ac
goodwinnk <goodwinnk@gmail.com>
2013-01-09 01:16:20 +0400

    Refactoring. Remove warnings about non-nullable fields.

--- compiler/frontend/src/org/jetbrains/jet/lang/types/TypeProjection.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/types/TypeProjection.java
@@ -19,7 +19,10 @@ package org.jetbrains.jet.lang.types;
 import org.jetbrains.annotations.NotNull;
 
 public class TypeProjection {
+    @NotNull
     private final Variance projection;
+
+    @NotNull
     private final JetType type;
 
     public TypeProjection(@NotNull Variance projection, @NotNull JetType type) {
@@ -27,7 +30,7 @@ public class TypeProjection {
         this.type = type;
     }
 
-    public TypeProjection(JetType type) {
+    public TypeProjection(@NotNull JetType type) {
         this(Variance.INVARIANT, type);
     }
 
@@ -43,10 +46,7 @@ public class TypeProjection {
 
     @Override
     public String toString() {
-        if (projection == Variance.INVARIANT) {
-            return type.toString();
-        }
-        return projection + " " + type;
+        return projection == Variance.INVARIANT ? type.toString() : projection + " " + type;
     }
 
     @Override
@@ -56,16 +56,11 @@ public class TypeProjection {
 
         TypeProjection that = (TypeProjection) o;
 
-        if (projection != that.projection) return false;
-        if (type != null ? !type.equals(that.type) : that.type != null) return false;
-
-        return true;
+        return that.projection == projection && type.equals(that.type);
     }
 
     @Override
     public int hashCode() {
-        int result = projection != null ? projection.hashCode() : 0;
-        result = 31 * result + (type != null ? type.hashCode() : 0);
-        return result;
+        return 31 * projection.hashCode() + type.hashCode();
     }
 }
