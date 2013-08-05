15497c0523246a0f6ccd15285ae004095dba6de0
Pavel V. Talanov <talanov.pavel@gmail.com>
2013-08-05 16:47:19 +0400

    Stubs for JetClassBody
    This fixes some cases in getParent() behaved differently on stubs and ast

--- compiler/frontend/src/org/jetbrains/jet/lang/psi/stubs/elements/JetStubElementTypes.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/psi/stubs/elements/JetStubElementTypes.java
@@ -31,4 +31,5 @@ public interface JetStubElementTypes {
     JetTypeParameterElementType TYPE_PARAMETER = new JetTypeParameterElementType("TYPE_PARAMETER");
     JetTypeParameterListElementType TYPE_PARAMETER_LIST = new JetTypeParameterListElementType("TYPE_PARAMETER_LIST");
     JetAnnotationElementType ANNOTATION_ENTRY = new JetAnnotationElementType("ANNOTATION_ENTRY");
+    JetClassBodyStubType CLASS_BODY = new JetClassBodyStubType("CLASS_BODY");
 }
