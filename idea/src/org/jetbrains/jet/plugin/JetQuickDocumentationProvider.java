341acf007c1dced54f63107c7688d1e4e7a21bf9
Pavel V. Talanov <talanov.pavel@gmail.com>
2013-08-05 20:09:43 +0400

    Fix logic in JetQuickDocumentationProvider

--- idea/src/org/jetbrains/jet/plugin/JetQuickDocumentationProvider.java
+++ idea/src/org/jetbrains/jet/plugin/JetQuickDocumentationProvider.java
@@ -19,12 +19,9 @@ package org.jetbrains.jet.plugin;
 import com.google.common.base.Predicate;
 import com.intellij.lang.documentation.AbstractDocumentationProvider;
 import com.intellij.lang.java.JavaDocumentationProvider;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiWhiteSpace;
-import com.intellij.psi.impl.compiled.ClsClassImpl;
-import com.intellij.psi.impl.compiled.ClsFileImpl;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
@@ -35,12 +32,8 @@ import org.jetbrains.jet.lang.descriptors.DeclarationDescriptor;
 import org.jetbrains.jet.lang.psi.*;
 import org.jetbrains.jet.lang.resolve.BindingContext;
 import org.jetbrains.jet.lang.resolve.BindingContextUtils;
-import org.jetbrains.jet.plugin.libraries.JetDecompiledData;
 import org.jetbrains.jet.plugin.project.WholeProjectAnalyzerFacade;
-import org.jetbrains.jet.plugin.references.BuiltInsReferenceResolver;
 import org.jetbrains.jet.renderer.DescriptorRenderer;
-
-import java.util.Collection;
 
 public class JetQuickDocumentationProvider extends AbstractDocumentationProvider {
     private static final Predicate<PsiElement> SKIP_WHITESPACE_AND_EMPTY_NAMESPACE = new Predicate<PsiElement>() {
@@ -63,7 +56,7 @@ public class JetQuickDocumentationProvider extends AbstractDocumentationProvider
             if (ref != null) {
                 DeclarationDescriptor declarationDescriptor = bindingContext.get(BindingContext.REFERENCE_TARGET, ref);
                 if (declarationDescriptor != null) {
-                    return render(declarationDescriptor, bindingContext, element, originalElement, mergeKotlinAndJava);
+                    return render(declarationDescriptor, element, originalElement, mergeKotlinAndJava);
                 }
                 if (declarationPsiElement != null) {
                     declarationPsiElement = BindingContextUtils.resolveToDeclarationPsiElement(bindingContext, ref);
@@ -75,7 +68,7 @@ public class JetQuickDocumentationProvider extends AbstractDocumentationProvider
                                                                                  declarationPsiElement);
 
                 if (declarationDescriptor != null) {
-                    return render(declarationDescriptor, bindingContext, element, originalElement, mergeKotlinAndJava);
+                    return render(declarationDescriptor, element, originalElement, mergeKotlinAndJava);
                 }
             }
             return "Unresolved";
@@ -84,10 +77,11 @@ public class JetQuickDocumentationProvider extends AbstractDocumentationProvider
     }
 
     private static String render(
-            @NotNull DeclarationDescriptor declarationDescriptor, @NotNull BindingContext bindingContext,
-            PsiElement element, PsiElement originalElement, boolean mergeKotlinAndJava) {
+            @NotNull DeclarationDescriptor declarationDescriptor,
+            PsiElement element, PsiElement originalElement, boolean mergeKotlinAndJava
+    ) {
         String renderedDecl = DescriptorRenderer.HTML.render(declarationDescriptor);
-        if (isKotlinDeclaration(declarationDescriptor, bindingContext, originalElement.getProject())) {
+        if (element.getLanguage() == JetLanguage.INSTANCE) {
             KDoc comment = findElementKDoc(element);
             if (comment != null) {
                 renderedDecl = renderedDecl + "<br/>" + kDocToHtml(comment);
@@ -103,21 +97,6 @@ public class JetQuickDocumentationProvider extends AbstractDocumentationProvider
             return renderedDecl;
         }
         return null;
-    }
-
-    private static boolean isKotlinDeclaration(DeclarationDescriptor descriptor, BindingContext bindingContext, Project project) {
-        PsiElement declaration = BindingContextUtils.descriptorToDeclaration(bindingContext, descriptor);
-        if (declaration == null) {
-            BuiltInsReferenceResolver libraryReferenceResolver = project
-                    .getComponent(BuiltInsReferenceResolver.class);
-            Collection<PsiElement> elements = libraryReferenceResolver.resolveStandardLibrarySymbol(descriptor);
-            return !elements.isEmpty();
-        }
-
-        if (JetLanguage.INSTANCE == declaration.getLanguage()) return true;
-        ClsClassImpl clsClass = PsiTreeUtil.getParentOfType(declaration, ClsClassImpl.class);
-        if (clsClass == null) return false;
-        return JetDecompiledData.isKotlinFile((ClsFileImpl) clsClass.getContainingFile());
     }
 
     @Override
