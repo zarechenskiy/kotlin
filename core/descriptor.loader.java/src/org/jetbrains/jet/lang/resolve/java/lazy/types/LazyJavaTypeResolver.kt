33a7007d3c1cf02003781e98abe17e0b6e5aa65c
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-10-30 15:36:01 +0400

    Account for fields annotated @NotNull

--- core/descriptor.loader.java/src/org/jetbrains/jet/lang/resolve/java/lazy/types/LazyJavaTypeResolver.kt
+++ core/descriptor.loader.java/src/org/jetbrains/jet/lang/resolve/java/lazy/types/LazyJavaTypeResolver.kt
@@ -258,7 +258,7 @@ class LazyJavaTypeAttributes(
         c: LazyJavaResolverContext,
         val annotationOwner: JavaAnnotationOwner,
         override val howThisTypeIsUsed: TypeUsage,
-        computeHowThisTypeIsUsedAccrodingToAnnotations: () -> TypeUsage
+        computeHowThisTypeIsUsedAccrodingToAnnotations: () -> TypeUsage = {howThisTypeIsUsed}
 ): JavaTypeAttributes {
 
     override val howThisTypeIsUsedAccordingToAnnotations: TypeUsage by c.storageManager.createLazyValue(
