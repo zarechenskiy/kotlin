1d2273fcadf3a7fde27da95c9f34030bb2e4bb13
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-11-13 22:44:38 +0400

    Record Java packages in the trace Account for LazyJavaPackageFragment in FindUsages

--- idea/src/org/jetbrains/jet/plugin/findUsages/JetUsageTypeProvider.kt
+++ idea/src/org/jetbrains/jet/plugin/findUsages/JetUsageTypeProvider.kt
@@ -32,6 +32,7 @@ import org.jetbrains.jet.lexer.JetTokens
 import org.jetbrains.jet.plugin.project.AnalyzerFacadeWithCache
 import org.jetbrains.jet.plugin.JetBundle
 import org.jetbrains.jet.lang.resolve.java.descriptor.JavaNamespaceDescriptor
+import org.jetbrains.jet.lang.resolve.java.lazy.descriptors.LazyJavaPackageFragment
 import org.jetbrains.jet.lang.resolve.DescriptorUtils
 
 public object JetUsageTypeProvider : UsageTypeProviderEx {
@@ -186,7 +187,8 @@ public object JetUsageTypeProvider : UsageTypeProviderEx {
             } else {
                 getClassUsageType()
             }
-            is JavaNamespaceDescriptor -> getClassUsageType()
+            // TODO Leaving both JavaNamespaceDescriptor and LazyJavaPackageFragment for compatibility, the former should be removed after switching to lazy JDR
+            is JavaNamespaceDescriptor, is LazyJavaPackageFragment  -> getClassUsageType()
             is VariableDescriptor -> getVariableUsageType()
             is FunctionDescriptor -> getFunctionUsageType(descriptor)
             else -> null
