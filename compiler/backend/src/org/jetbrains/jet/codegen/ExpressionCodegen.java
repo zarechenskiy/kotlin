794ed32696bdf1a05379e3a2eecf1b4b79f0ac8e
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2014-01-21 11:17:04 +0400

    CaprutedParamInfo refactoring

--- compiler/backend/src/org/jetbrains/jet/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/jet/codegen/ExpressionCodegen.java
@@ -32,6 +32,7 @@ import org.jetbrains.asm4.MethodVisitor;
 import org.jetbrains.asm4.Type;
 import org.jetbrains.asm4.commons.InstructionAdapter;
 import org.jetbrains.asm4.commons.Method;
+import org.jetbrains.asm4.tree.MethodNode;
 import org.jetbrains.jet.codegen.asm.InlineCodegen;
 import org.jetbrains.jet.codegen.asm.InlineCodegenUtil;
 import org.jetbrains.jet.codegen.asm.Inliner;
@@ -88,7 +89,7 @@ public class ExpressionCodegen extends JetVisitor<StackValue, StackValue> implem
 
     private int myLastLineNumber = -1;
 
-    final InstructionAdapter v;
+    InstructionAdapter v;
     final MethodVisitor methodVisitor;
     final FrameMap myFrameMap;
     final JetTypeMapper typeMapper;
