3f505995726993115f66c5ad4873f9e0e048987b
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2014-02-05 17:11:01 +0400

    New try/catch tests

--- compiler/testData/codegen/boxInline/tryCatchFinally/2.kt
+++ compiler/testData/codegen/boxInline/tryCatchFinally/2.kt
@@ -1,6 +1,19 @@
 class My(val value: Int)
 
-inline fun <T, R> T.perform(job: (T)-> R) : R {
-    return job(this)
+inline fun <T, R> T.performWithFinally(job: (T)-> R, finally: (T) -> R) : R {
+    try {
+        return job(this)
+    } finally {
+        return finally(this)
+    }
 }
 
+inline fun <T, R> T.performWithFailFinally(job: (T)-> R, failJob : (e: RuntimeException, T) -> R, finally: (T) -> R) : R {
+    try {
+        return job(this)
+    } catch (e: RuntimeException) {
+        return failJob(e, this)
+    } finally {
+        return finally(this)
+    }
+}
