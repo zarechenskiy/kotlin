8ce307321241b31ff892de2efdfdbcdcc3015451
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-02-08 02:20:26 +0400

    Remove obsolete code

--- libraries/tools/kotlin-stdlib-gen/src/templates/Primitives.kt
+++ libraries/tools/kotlin-stdlib-gen/src/templates/Primitives.kt
@@ -22,25 +22,3 @@ private fun PrimitiveType.zero() = when (this) {
     PrimitiveType.Float -> "0.toFloat()"
     else -> throw IllegalArgumentException("Primitive type $this doesn't have default value")
 }
-
-private fun returnTypeForSum(primitive: PrimitiveType) = when (primitive) {
-    PrimitiveType.Byte -> PrimitiveType.Int
-    PrimitiveType.Short -> PrimitiveType.Int
-    else -> primitive
-}
-
-fun sumFunction(primitive: PrimitiveType) =
-        f("sum()") {
-            doc = "Sums up the elements"
-            makeInline = false
-            if (returnTypeForSum(primitive) != primitive) {
-                //this is required to avoid clash of erasured method signatures (Iterables.sum(): Int)
-                //absentFor(Iterables)
-            }
-            Iterables.customReceiver("Iterable<${primitive.name}>")
-            ArraysOfObjects.customReceiver("Array<${primitive.name}>")
-            returns(returnTypeForSum(primitive).name)
-            body {
-                "return fold(${primitive.zero()}, {a,b -> a+b})"
-            }
-        }
