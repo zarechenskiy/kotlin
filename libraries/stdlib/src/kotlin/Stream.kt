e90af77a3c2bfa0d37bb7b62981ab78ae1571c9c
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-02-07 23:10:38 +0400

    Add basic Stream

---
+++ libraries/stdlib/src/kotlin/Stream.kt
@@ -0,0 +1,89 @@
+package kotlin
+
+import kotlin.support.AbstractIterator
+
+public trait Stream<out T> {
+    fun iterator(): Iterator<T>
+}
+
+public fun <T> Iterable<T>.stream(): Stream<T> = object : Stream<T> {
+    override fun iterator(): Iterator<T> {
+        return this@stream.iterator()
+    }
+}
+
+class FilteringStream<T>(val stream: Stream<T>, val sendWhen: Boolean = true, val predicate: (T) -> Boolean) : Stream<T> {
+    override fun iterator(): Iterator<T> = object : AbstractIterator<T>() {
+        val iterator = stream.iterator()
+        override fun computeNext() {
+            while (iterator.hasNext()) {
+                val item = iterator.next()
+                if (predicate(item) == sendWhen) {
+                    setNext(item)
+                    return
+                }
+            }
+            done()
+        }
+    }
+}
+
+class TransformingStream<T, R>(val stream: Stream<T>, val transformer: (T) -> R) : Stream<R> {
+    override fun iterator(): Iterator<R> = object : AbstractIterator<R>() {
+        val iterator = stream.iterator()
+        override fun computeNext() {
+            if (iterator.hasNext()) {
+                setNext(transformer(iterator.next()))
+            } else {
+                done()
+            }
+        }
+    }
+}
+
+class LimitedStream<T>(val stream: Stream<T>, val stopWhen: Boolean = true, val predicate: (T) -> Boolean) : Stream<T> {
+    override fun iterator(): Iterator<T> = object : AbstractIterator<T>() {
+        val iterator = stream.iterator()
+        override fun computeNext() {
+            if (!iterator.hasNext()) {
+                done()
+            } else {
+                val item = iterator.next()
+                if (predicate(item) == stopWhen) {
+                    done()
+                } else {
+                    setNext(item)
+                }
+            }
+        }
+    }
+}
+
+class FunctionStream<T : Any>(val producer: () -> T?) : Stream<T> {
+    override fun iterator(): Iterator<T> = object : AbstractIterator<T>() {
+
+        override fun computeNext() {
+            val item = producer()
+            if (item == null) {
+                done()
+            } else {
+                setNext(item)
+            }
+        }
+    }
+}
+
+/**
+ * Returns a stream which invokes the function to calculate the next value on each iteration until the function returns *null*
+ */
+public fun <T:Any> stream(nextFunction: () -> T?) : Stream<T> {
+    return FunctionStream(nextFunction)
+}
+
+/**
+ * Returns a stream which invokes the function to calculate the next value based on the previous one on each iteration
+ * until the function returns *null*
+ */
+public /*inline*/ fun <T: Any> stream(initialValue: T, nextFunction: (T) -> T?): Stream<T> =
+        stream(nextFunction.toGenerator(initialValue))
+
