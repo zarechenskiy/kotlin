0e4b1281ade9677107e1baaa24655895b719124c
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2014-02-14 03:00:00 +0400

    -- another lib

--- idea/tests/org/jetbrains/jet/search/JUnitMembersSearcherTest.java
+++ idea/tests/org/jetbrains/jet/search/JUnitMembersSearcherTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2013 JetBrains s.r.o.
+ * Copyright 2010-2014 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ import java.util.List;
 
 public class JUnitMembersSearcherTest extends AbstractSearcherTest {
     private static final LightProjectDescriptor junitProjectDescriptor =
-            new JetJdkAndLibraryProjectDescriptor(new File(PathManager.getHomePath().replace(File.separatorChar, '/') + "/lib/junit-4.10.jar"));
+            new JetJdkAndLibraryProjectDescriptor(new File(PathManager.getHomePath().replace(File.separatorChar, '/') + "/lib/junit-4.11.jar"));
 
     public void testJunit3() throws IOException {
         doJUnit3test();
