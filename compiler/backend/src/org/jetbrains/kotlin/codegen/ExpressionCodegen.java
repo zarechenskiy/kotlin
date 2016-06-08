7e8b94834aebcf2acada2b9fbc912a47ffb98e0c
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-06-08 18:32:57 +0300

    Move classes that are using to specialize types to TypeSpecializer

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
@@ -2982,6 +2982,8 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
 
             boolean isReified = key.isReified() || InlineUtil.isArrayConstructorWithLambda(resolvedCall.getResultingDescriptor());
 
+            boolean isAnyfied = TypeUtils.isAnyfiedTypeParameter(key);
+
             Pair<TypeParameterDescriptor, ReificationArgument> typeParameterAndReificationArgument = extractReificationArgument(type);
             if (typeParameterAndReificationArgument == null) {
                 KotlinType approximatedType = CapturedTypeApproximationKt.approximateCapturedTypes(entry.getValue()).getUpper();
@@ -2990,12 +2992,12 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
                 Type asmType = typeMapper.mapTypeParameter(approximatedType, signatureWriter);
 
                 mappings.addParameterMappingToType(
-                        key.getName().getIdentifier(), approximatedType, asmType, signatureWriter.toString(), isReified
+                        key.getName().getIdentifier(), approximatedType, asmType, signatureWriter.toString(), isReified, isAnyfied
                 );
             }
             else {
                 mappings.addParameterMappingForFurtherReification(
-                        key.getName().getIdentifier(), type, typeParameterAndReificationArgument.getSecond(), isReified
+                        key.getName().getIdentifier(), type, typeParameterAndReificationArgument.getSecond(), isReified, isAnyfied
                 );
             }
         }
