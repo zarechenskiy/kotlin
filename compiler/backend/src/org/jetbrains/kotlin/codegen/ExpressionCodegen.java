be6e5653e75de83d670b56a223e2c3a3423a86d5
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-06-08 04:10:40 +0300

    ++ Generalization: ReifiedTypeParametersUsages

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
@@ -226,7 +226,7 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
         propagateChildReifiedTypeParametersUsages(literalCodegen.getReifiedTypeParametersUsages());
 
         return new ObjectLiteralResult(
-                literalCodegen.getReifiedTypeParametersUsages().wereUsedReifiedParameters(),
+                literalCodegen.getReifiedTypeParametersUsages().wereUsedSpecializedParameters(),
                 classDescriptor
         );
     }
@@ -236,7 +236,7 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
             for (TypeProjection supertypeArgument : type.getArguments()) {
                 TypeParameterDescriptor parameterDescriptor = TypeUtils.getTypeParameterDescriptorOrNull(supertypeArgument.getType());
                 if (parameterDescriptor != null && parameterDescriptor.isReified()) {
-                    member.getReifiedTypeParametersUsages().addUsedReifiedParameter(parameterDescriptor.getName().asString());
+                    member.getReifiedTypeParametersUsages().addUsedSpecializedParameter(parameterDescriptor.getName().asString());
                 }
             }
         }
@@ -1445,7 +1445,7 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
 
         closureCodegen.generate();
 
-        if (closureCodegen.getReifiedTypeParametersUsages().wereUsedReifiedParameters()) {
+        if (closureCodegen.getReifiedTypeParametersUsages().wereUsedSpecializedParameters()) {
             TypeSpecializer.putNeedClassSpecializationMarker(v, TypeSpecializationKind.REIFICATION);
             propagateChildReifiedTypeParametersUsages(closureCodegen.getReifiedTypeParametersUsages());
         }
@@ -3997,7 +3997,7 @@ The "returned" value of try expression with no finally is either the last expres
             TypeParameterDescriptor typeParameterDescriptor = typeParameterAndReificationArgument.getFirst();
             if (typeParameterDescriptor.getContainingDeclaration() != context.getContextDescriptor()) {
                 parentCodegen.getReifiedTypeParametersUsages().
-                        addUsedReifiedParameter(typeParameterDescriptor.getName().asString());
+                        addUsedSpecializedParameter(typeParameterDescriptor.getName().asString());
             }
             v.iconst(operationKind.getId());
             v.visitLdcInsn(typeParameterAndReificationArgument.getSecond().asString());
--- compiler/backend/src/org/jetbrains/kotlin/codegen/inline/AsmTypeRemapper.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inline/AsmTypeRemapper.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ class AsmTypeRemapper(val typeRemapper: TypeRemapper, val isDefaultGeneration: B
 
                 if (mapping.newName != null) {
                     if (mapping.isReified) {
-                        result.reifiedTypeParametersUsages.addUsedReifiedParameter(mapping.newName)
+                        result.reifiedTypeParametersUsages.addUsedSpecializedParameter(mapping.newName)
                     }
                     return super.visitTypeVariable(mapping.newName)
                 }
