644db38286a5524992c67e419f24eabaa3082098
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-06-10 12:31:01 +0300

    Specialize array iteration for value types

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
@@ -1022,7 +1022,11 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
         protected void assignToLoopParameter() {
             Type arrayElParamType;
             if (KotlinBuiltIns.isArray(loopRangeType)) {
-                arrayElParamType = boxType(asmElementType);
+                if (KotlinBuiltIns.isPrimitiveValueType(elementType)) {
+                    arrayElParamType = asmElementType;
+                } else {
+                    arrayElParamType = boxType(asmElementType);
+                }
             }
             else {
                 arrayElParamType = asmElementType;
