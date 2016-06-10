2cbcbf251a7902ad5340d15072fe491bfa666461
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-06-10 15:14:02 +0300

    Several tests on lambda specialization

---
+++ compiler/testData/codegen/box/specialization/functions/lambdaWithDifferentReturnType.kt
@@ -0,0 +1,18 @@
+inline fun <@Anyfied T, @Anyfied R> takeLambda(el: T, l: (T) -> R): R {
+    return l(el)
+}
+
+fun box(): String {
+    takeLambda(1 as vInt) {
+        1 as vInt
+    }
+
+    return "OK"
+}
+
+// 0 CHECKCAST
+// 0 intValue
+// 3 ALOAD
+// 2 ISTORE
+// 1 ILOAD
+// 0 ASTORE
