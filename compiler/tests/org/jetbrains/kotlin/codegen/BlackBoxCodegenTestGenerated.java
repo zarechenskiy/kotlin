6a36d7c598a5138a697408c2b009732ef4a42e90
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-06-22 13:53:06 +0300

    ++ tests (currently these tests are failing)

--- compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
@@ -15198,6 +15198,12 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("nonAnyfiedFunFromAnyfied.kt")
+            public void testNonAnyfiedFunFromAnyfied() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/specialization/functions/nonAnyfiedFunFromAnyfied.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("nullableValueAfterCheck.kt")
             public void testNullableValueAfterCheck() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/specialization/functions/nullableValueAfterCheck.kt");
