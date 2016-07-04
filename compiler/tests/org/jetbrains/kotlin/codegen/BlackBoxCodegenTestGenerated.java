51e0f66b31af472f5824c7791f8c31558ae92df4
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-07-04 17:50:58 +0300

    Specialize intrinsic 'get' functions for arrays

--- compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
@@ -13533,6 +13533,12 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("anyfiedInObject.kt")
+            public void testAnyfiedInObject() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/specialization/functions/anyfiedInObject.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("anyfiedMap.kt")
             public void testAnyfiedMap() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/specialization/functions/anyfiedMap.kt");
