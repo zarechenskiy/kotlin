772e4680d19d0a47537dd5a55e5155be02f2779c
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-07-05 15:15:52 +0300

    Do not put markers for specialized arrays

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
@@ -353,7 +353,8 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
             if (expr instanceof KtExpression &&
                 !(expr instanceof KtIfExpression) &&
                 !(expr instanceof KtCallExpression) &&
-                !(expr instanceof KtBlockExpression)) {
+                !(expr instanceof KtBlockExpression) &&
+                !(expr instanceof KtDotQualifiedExpression)) {
 
                 expressionType = expressionJetType((KtExpression) expr);
                 if (expressionType != null && TypeUtils.isAnyfiedTypeParameter(expressionType)) {
@@ -2946,7 +2947,9 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
             receiver = StackValue.receiverWithTask(resolvedCall, receiver, this, callableMethod, new Function1<KotlinType, Unit>() {
                 @Override
                 public Unit invoke(KotlinType kotlinType) {
-                    putAnyfiedOperationMarkerIfTypeIsReifiedParameter(kotlinType);
+                    if (TypeUtils.isAnyfiedTypeParameter(kotlinType)) {
+                        putAnyfiedOperationMarkerIfTypeIsReifiedParameter(kotlinType);
+                    }
 
                     return Unit.INSTANCE;
                 }
