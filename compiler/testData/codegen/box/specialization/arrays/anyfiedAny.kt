e63d778fad4a06bedbc1862e0875692585b6a50a
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-07-06 13:36:05 +0300

    Several tests: anyfied version of functions from stdlib

---
+++ compiler/testData/codegen/box/specialization/arrays/anyfiedAny.kt
@@ -0,0 +1,18 @@
+// WITH_RUNTIME
+
+fun box(): String {
+    val arr = intArrayOf(1, 2) as Array<vInt>
+
+    val anyResult1 = arr.anyfiedAny { true }
+    val anyResult2 = arr.anyfiedAny { false }
+
+    assert(anyResult1 == true)
+    assert(anyResult2 == false)
+
+    return "OK"
+}
+
+inline fun <@Anyfied T> Array<out T>.anyfiedAny(predicate: (T) -> Boolean): Boolean {
+    for (element in this) if (predicate(element)) return true
+    return false
+}
---
+++ compiler/testData/codegen/box/specialization/arrays/firstOrElse.kt
@@ -0,0 +1,17 @@
+// WITH_RUNTIME
+
+fun box(): String {
+    val arr = intArrayOf(1, 2) as Array<vInt>
+    val default = 10 as vInt
+
+    val f = arr.firstOrElse(default) { false }
+
+    assert(f == default)
+
+    return "OK"
+}
+
+inline fun <@Anyfied T> Array<out T>.firstOrElse(default: T, predicate: (T) -> Boolean): T {
+    for (element in this) if (predicate(element)) return element
+    return default
+}
