d34a4f46b2fb96ed2e693b4e0764ad68e3722a21
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-08-23 13:52:14 +0300

    Sample test on value of double

--- compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
@@ -14417,6 +14417,12 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/specialization/values/sample.kt");
                 doTest(fileName);
             }
+
+            @TestMetadata("valueOfDouble.kt")
+            public void testValueOfDouble() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/specialization/values/valueOfDouble.kt");
+                doTest(fileName);
+            }
         }
     }
 
