3859ac634e0c7791c6c242f2150fc159422ca9ef
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-08-23 17:20:16 +0300

    Add tests on simple class representing an unsigned int

--- compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
@@ -14418,6 +14418,12 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("unsignedInt.kt")
+            public void testUnsignedInt() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/specialization/values/unsignedInt.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("valueOfDouble.kt")
             public void testValueOfDouble() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/box/specialization/values/valueOfDouble.kt");
