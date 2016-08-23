aeead8c6f33a5067de12256576b45b90313ca9ed
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-08-23 17:25:53 +0300

    To test prettification

--- compiler/tests/org/jetbrains/kotlin/codegen/BytecodeTextTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/BytecodeTextTestGenerated.java
@@ -1366,6 +1366,12 @@ public class BytecodeTextTestGenerated extends AbstractBytecodeTextTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("constructorCall.kt")
+            public void testConstructorCall() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/bytecodeText/specialization/values/constructorCall.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("sample.kt")
             public void testSample() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/codegen/bytecodeText/specialization/values/sample.kt");
