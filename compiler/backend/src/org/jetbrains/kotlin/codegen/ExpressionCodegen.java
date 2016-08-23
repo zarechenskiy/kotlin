dd213116b2534ef4f6e516e553bde3daf21718fb
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-08-23 14:51:22 +0300

    Avoid initializing for value classes

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
@@ -2717,6 +2717,13 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
         FunctionDescriptor descriptor = accessibleFunctionDescriptor(resolvedCall);
 
         if (descriptor instanceof ConstructorDescriptor) {
+            DeclarationDescriptor containingDeclaration = descriptor.getContainingDeclaration();
+            if (containingDeclaration instanceof ClassDescriptor) {
+                if (((ClassDescriptor) containingDeclaration).isValue()) {
+                    KtValueArgument valueArgument = expression.getValueArguments().get(0);
+                    return gen(valueArgument.getArgumentExpression());
+                }
+            }
             return generateNewCall(expression, resolvedCall);
         }
 
