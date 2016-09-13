88d9030f4fb5ce23d031eb30044b5b2a529c2c0e
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-09-13 16:14:07 +0300

    Consider size of arguments when generating delegate to anyfied impl

--- compiler/backend/src/org/jetbrains/kotlin/codegen/FunctionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/FunctionCodegen.java
@@ -668,8 +668,10 @@ public class FunctionCodegen {
         iv.load(0, AsmTypes.OBJECT_TYPE);
         iv.visitFieldInsn(Opcodes.GETFIELD, fieldOwnerType.getInternalName(), fieldName, fieldType.getDescriptor());
 
+        int k = 1;
         for (int i = 1; i < argTypes.length; ++i) {
-            iv.load(i, argTypes[i]);
+            iv.load(k, argTypes[i]);
+            k += argTypes[i].getSize();
         }
 
         iv.invokestatic(classToDelegateTo, asmMethod.getName(), asmMethod.getDescriptor(), false);
