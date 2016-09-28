84338904d8be75deffe7ee122438c915c82e89df
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2016-09-28 12:13:03 +0300

    First test on Kotlin-Java override with value types

---
+++ compiler/testData/codegen/box/specialization/javaInterop/overridenValue.kt
@@ -0,0 +1,38 @@
+// WITH_RUNTIME
+
+// FILE: foo.kt
+
+value class Name(val s: String) {
+    operator fun box(): String = s
+    operator fun unbox(boxed: String): Name = Name(boxed)
+}
+
+interface SomeK {
+    fun sample(ls: List<Name>): Int
+}
+
+// FILE: A.java
+
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+public class A implements SomeK {
+    @Override
+    public int sample(@NotNull List<String> ls) {
+        return 100;
+    }
+}
+
+// FILE: 1.kt
+
+fun box(): String {
+    val name = Name("Kotlin")
+
+    val a = A()
+    val ls = listOf(name)
+
+    val result = a.sample(ls)
+    assert(result == 100)
+
+    return "OK"
+}
